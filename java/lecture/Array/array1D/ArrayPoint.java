290d4c308f8cf15ec5b67fb84a04affb9d41d96a
Arya <148077534+aryaadhinata@users.noreply.github.com>
2026-09-08 23:31:29 +0700

    add: new file java for array 1d

---
+++ java/lecture/Array/array1D/ArrayPoint.java
@@ -0,0 +1,21 @@
+import java.awt.Point;
+import java.util.Scanner;
+
+public class ArrayPoint{
+    public static void main(String args[]){
+        Point arrPoint[] = new Point[2];
+
+        int i = 0;
+        int x = 0;
+        int y = 0;
+
+        Scanner scan = new Scanner(System.in);
+
+        for(i = 0; i < arrPoint.length; i++){
+            x = scan.nextInt();
+            y = scan.nextInt();
+            arrPoint[i] = new Point(x, y);
+            System.out.println("x : " + arrPoint[i].x + " y: " + arrPoint[i].y);
+        }
+    }
+}
