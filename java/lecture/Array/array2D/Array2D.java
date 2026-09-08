126607dc670eb17acb5b2ae6e584f60e99c61a64
Arya <148077534+aryaadhinata@users.noreply.github.com>
2026-09-08 23:40:44 +0700

    add : new java file for array2D

---
+++ java/lecture/Array/array2D/Array2D.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Array2D{
+    public static void main(String[] args) {
+        int arr[][] = new int[2][3];
+        int i = 0, j = 0;
+
+        Scanner scan = new Scanner(System.in);
+
+        for(i = 0; i < arr.length; i++){
+            for(j = 0; j < arr[i].length; j++){
+                arr[i][j] =  scan.nextInt();
+                System.out.println(arr[i][j]);
+            }
+        }
+    }
+}
